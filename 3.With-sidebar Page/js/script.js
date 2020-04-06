be9f2ad10d8ab2d776f9d05fcf556b9c612a8956
senanoguz <53573382+senanoguz@users.noreply.github.com>
2020-04-06 18:58:26 +0400

    .
    .

---
+++ 3.With-sidebar Page/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    var order = $('#order')
+    function show(){
+        order.css('opacity','100')
+    }
+    function hide(){
+        order.css('opacity','0')
+    }
+})
